a83bd8f1ab1576996c333442dea01e743ca14095
GiMerguizo <giovana.pontes5654@hotmail.com>
2022-07-22 08:07:27 -0300

    Finalização do curso

---
+++ Santander Code Girls/Dominando IDEs Java/projetos-intellj/variaveis-dados-e-operadores/src/Main.java
@@ -0,0 +1,50 @@
+public class Main {
+    public static void main(String[] args) {
+        byte b1 = 0;
+        byte b2 = -100;
+
+        short s1 = 12;
+        short s2 = -12;
+
+        long l1 = 100000000L;
+        long l2 = 200000000L;
+
+        int i1 = -932;
+        int i2 = 213;
+
+        float f1 = 3.14f;
+        float f2 = 1.7583f;
+
+        double d1 = 123143.3421;
+        double d2 = 319313.324;
+
+        char c1 = 'V';
+        char c2 = 'F';
+
+        String string1 = "Verdadeiro";
+        String string2 = "Falso";
+
+        boolean bool1 = false;
+        boolean bool2 = true;
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(i1);
+        System.out.println(i2);
+        System.out.println(f1);
+        System.out.println(f2);
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(string1);
+        System.out.println(string2);
+        System.out.println(bool1);
+        System.out.println(bool2);
+
+    }
+}
