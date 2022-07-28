1f01674fe26d6a0039997a50097c376fb43af48b
GiMerguizo <giovana.pontes5654@hotmail.com>
2022-07-28 10:43:43 -0300

    Módulos em Java

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-# Cursos da Plataforma DIO
+# Cursos da Plataforma DIO :computer:
 Repositório feito para a organização dos estudos realizados na DIO
 
 ## Links Úteis
 [DIO](https://www.dio.me/)<br>
 [GitHub](https://github.com/)
+
---
+++ Santander Code Girls/Dominando IDEs Java/projetos-intellj/exercicios/src/modulos/operacoes.java
@@ -0,0 +1,13 @@
+package modulos;
+
+public class Main {
+    public class operacoes {
+        int num1, num2;
+               
+        
+        System.out.println(soma);
+        System.out.println(subtracao);
+        System.out.println(multiplicacao);
+        System.out.println(divisao);
+    }
+}
