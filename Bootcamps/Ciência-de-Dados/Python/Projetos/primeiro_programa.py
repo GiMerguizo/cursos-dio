d2a7bb2b392c33768461a80b8142dbc9c5207928
GiMerguizo <giovana.pontes5654@hotmail.com>
2022-09-19 10:07:05 -0300

    Updated primeiro_programa.py

--- Bootcamps/Ciência-de-Dados/Python/Projetos/primeiro_programa.py
+++ Bootcamps/Ciência-de-Dados/Python/Projetos/primeiro_programa.py
@@ -2,5 +2,8 @@ print("Oi, seja bem-vindo!")
 idade = int(input("Sua idade: "))
 nome = input("Seu nome: ")
 
-print('Olá,', nome, '! Você possui', idade, 'anos. Correto?')
+print('Olá,',nome,'! Você possui', idade, 'anos. Correto?')
 print('Olá, {}! Você possui {} anos. Correto? '.format(nome, idade))
+
+age, name = (23, 'Guilherme')
+print(f'Meu nome é {name} e eu tenho {age} ano(s) de idade.')
